1110a0404f5dbcda84408cddb2b13349bd31d846
Allen Gooch <allen.gooch@gmail.com>
2017-08-25 17:31:04 -0500

    Add new metex mix project.

---
+++ ch3/metex/lib/metex/worker.ex
@@ -0,0 +1,35 @@
+defmodule Metex.Worker do
+
+  def temperature_of(location) do
+    result = url_for(location) |> HTTPoison.get |> parse_response
+    case result do
+      {:ok, temp} ->
+        "#{location}: #{temp} degrees celsius"
+      :error ->
+        "#{location} not found"
+    end
+  end
+
+  defp url_for(location) do
+    location = URI.encode(location)
+    "http://api.openweathermap.org/data/2.5/weather?q=#{location}&appid=#{apikey}"
+  end
+
+  defp parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
+    body |> JSON.decode! |> compute_temperature
+  end
+  defp parse_response(_), do: :error
+
+  defp compute_temperature(json) do
+    try do
+      temp = (json["main"]["temp"] - 273.15) |> Float.round(1)
+      {:ok, temp}
+    rescue
+      _ -> :error
+    end
+  end
+
+  defp apikey do
+    "50e13dd2d6e18149f7ae4e50b4cc04ea"
+  end
+end
